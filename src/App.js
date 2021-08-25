d0023fbf4dfd0124c84f92c892c8712b05c9ce83
kumar <shubham.u.kumar@capgemini.com>
2021-08-26 00:40:34 +0530

    MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import { exportComponentAsPNG } from "react-component-export-image";
 
 function App() {
+  const [input, setInput] = useState("");
+  const componentRef = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <div className="sidebar">
+          <input
+            className="input"
+            placeholder="Enter your name"
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+
+          <button
+            disabled={input === ""}
+            onClick={(e) => {
+              e.preventDefault();
+              exportComponentAsPNG(componentRef);
+            }}
+          >
+            Download
+          </button>
+        </div>
+        <div className="mainbar" ref={componentRef}>
+          <h1>{input}</h1>
+          <img
+            className="image1"
+            src="https://i.imgur.com/xa3WSGC.png"
+            alt="certificate"
+          />
+        </div>
+      </main>
+      <footer>
+        <h1 className="notes">Made with ❤️ by Shubham</h1>
+      </footer>
+    </>
   );
 }
 
